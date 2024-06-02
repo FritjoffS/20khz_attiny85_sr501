34b279208a2910f1007ec120ce8c22efcf3493e1
FritjoffS <attholmen@gmail.com>
2024-06-02 14:43:58 +0200

    working version

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,26 @@
 #include <Arduino.h>
 
-const int speakerPin = 5; // Change this to the PWM-capable pin you've connected the speaker to
+const int speakerPin = 1; // Change this to the PWM-capable pin you've connected the speaker to
+const int pirPin = A1; // Define the pin connected to the PIR sensor
 const unsigned int frequency = 20000; // 20 kHz
 
 void setup()
 {
     pinMode(speakerPin, OUTPUT);
-    Serial.begin(115200);
-
+    pinMode(pirPin, INPUT);
 }
 
 void loop()
 {
-    analogWrite(speakerPin, 255); // Adjust this value to increase or decrease volume (0-255)
-    tone(speakerPin, frequency);
-    Serial.print("tone on");
-    delay(20000);
+    int pirState = digitalRead(pirPin); // Read the state of the PIR sensor
+
+    if (pirState == HIGH) // If motion is detected
+    {
+        tone(speakerPin, frequency); // Generate 20 kHz tone on the specified pin
+        delay(10000); // Keep the tone on for 10 seconds
+        noTone(speakerPin); // Turn off the tone
+    }
+    
+    delay(500); // Small delay to avoid multiple triggers
 }
+    
